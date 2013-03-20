2f4eef89e859a3169304f623e6f5381dfa06e609
Evgeny Smirnov <smirik@gmail.com>
2013-03-20 08:11:40 +0400

    Add strtolower transformation when check answers

--- Model/UserQuestion.php
+++ Model/UserQuestion.php
@@ -4,47 +4,30 @@ namespace Smirik\QuizBundle\Model;
 
 use Smirik\QuizBundle\Model\om\BaseUserQuestion;
 
-
-/**
- * Skeleton subclass for representing a row from the 'users_questions' table.
- *
- * 
- *
- * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
- * long as it does not already exist in the output directory.
- *
- * @package    propel.generator.vendor.bundles.Smirik.QuizBundle.Model
- */
-class UserQuestion extends BaseUserQuestion {
-
-  public function addAnswer($answer, $answer_text = false)
-  {
-    /**
-     * Check is this answer for question we need
-     */
-    if ($this->getQuestion()->getId() != $answer->getQuestionId())
+class UserQuestion extends BaseUserQuestion
+{
+    public function addAnswer($answer, $answer_text = false)
     {
-      throw new \Exception ('Question and Answer does not respond');
+        /**
+         * Check is this answer for question we need
+         */
+        if ($this->getQuestion()->getId() != $answer->getQuestionId()) {
+            throw new \Exception ('Question and Answer does not respond');
+        }
+
+        if ($answer_text !== false) {
+            if (mb_strtolower($answer->getIsRight()) == mb_strtolower($answer_text)) {
+                $this->setIsRight(true);
+            } else {
+                $this->setIsRight(false);
+            }
+            $this->setAnswerText($answer_text);
+        } else {
+            $this->setIsRight($answer->getIsRight());
+        }
+        $this->setAnswer($answer);
+        $this->setAnswerText($answer_text);
+        $this->save();
     }
-
-    if ($answer_text !== false)
-    {
-      if ($answer->getIsRight() == $answer_text)
-      {
-        $this->setIsRight(true);
-      } else
-      {
-        $this->setIsRight(false);
-      }
-      $this->setAnswerText($answer_text);
-    } else
-    {
-      $this->setIsRight($answer->getIsRight());
-    }
-    $this->setAnswer($answer);
-    $this->setAnswerText($answer_text);
-    $this->save();
-  }
 
 } // UserQuestion
